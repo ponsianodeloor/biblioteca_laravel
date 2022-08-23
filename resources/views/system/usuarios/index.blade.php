08f2c49c8e505069f31a81500853298974499be9
Ponsiano De Loor <ponsianodeloor@gmail.com>
2022-08-23 14:15:06 -0500

    mostrar leyenda para los usuarios

--- resources/views/system/usuarios/index.blade.php
+++ resources/views/system/usuarios/index.blade.php
@@ -7,7 +7,7 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <p>Aqui se muestran a los usuarios que realizan los prestamos de los libros.</p>
 @stop
 
 @section('css')
